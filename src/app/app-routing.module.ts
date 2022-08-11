5f702bb488a554eea336a5cf92568fee389d810f
angel.borrego@gmail <angel.borrego@gmail.com>
2022-08-11 17:54:11 -0400

    fin seccion 3 - diseño

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficalComponent } from './pages/grafical/grafical.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+const routes: Routes = [
+  { path:'', 
+    component: PagesComponent,
+    children: [
+      {path:'dashboard', component:DashboardComponent},
+      {path:'progress', component:ProgressComponent},
+      {path:'grafical',component:GraficalComponent},
+      {path:'', redirectTo:'/dashboard', pathMatch:'full'}]
+  },  
+
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+
+  {path:'**', component: NopagefoundComponent}
+
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
